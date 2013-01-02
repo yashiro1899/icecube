93c985d6f61c6bf9d11f0b9250c689f65a483878
Ice White <yashiro1899@gmail.com>
2013-01-03 01:51:50 +0800

    added server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// Just a basic server setup for this site
+var Stack = require('stack'),
+    Creationix = require('creationix'),
+    Http = require('http');
+
+Http.createServer(Stack(
+Creationix.log(),
+require('wheat')(__dirname + "/.."))).listen(80);
+
