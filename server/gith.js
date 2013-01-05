1b83f4af2b4e9601b12413527833bbfb0be9db97
Ice White <yashiro1899@gmail.com>
2013-01-05 11:57:52 +0800

    added the date for gith

--- server/gith.js
+++ server/gith.js
@@ -6,6 +6,7 @@ console.log('Listening on port 9527 for GitHub WebHook...');
 GitH({
     repo: 'yashiro1899/icecube'
 }).on('all', function(payload) {
+    console.log('\n[' + new Date() + ']');
     console.log('head_id: ' + payload.sha);
 
     ChildProcess.execFile('/home/ec2-user/icecube/post-receive.sh', [], {}, function(err, stdout, stderr) {
