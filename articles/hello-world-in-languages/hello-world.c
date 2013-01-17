3750f1cf8a50f3e10013f18e5823bf910d1e6c3d
Ice White <yashiro1899@gmail.com>
2013-01-17 19:50:47 +0800

    hello-world-in-languages

---
+++ articles/hello-world-in-languages/hello-world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main()
+{
+    for(;;)
+    { 
+        printf ("Hello World!\n");
+    }
+}
---
+++ articles/hello-world-in-languages/hello-world.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main (String args[]) {
+        for (;;) {
+            System.out.print("Hello World ");
+        }
+    }
+}
