d47fa6dd85ebb7eeaa4a859d6a5bff69b9871380
Ice White <yashiro1899@gmail.com>
2013-01-19 09:44:10 +0800

    deleted the node_modules/creationix/

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 // Just a basic server setup for this site
 var Stack = require('stack'),
-    Creationix = require('creationix'),
     Http = require('http');
 
-Http.createServer(Stack(
-Creationix.log(),
-require('wheat')(__dirname + "/.."))).listen(process.env.NODE_DEV ? 8000 : 80);
+var special = {
+    "Content-Type": true,
+    "Location": true,
+    "Content-Length": true,
+    "Content-Range": true
+};
 
+Http.createServer(Stack(function(req, res, next) {
+    var writeHead = res.writeHead;
+    var start = Date.now();
+
+    res.writeHead = function(code, headers) {
+        var extra = [];
+        if (headers) {
+            Object.keys(headers).forEach(function(key) {
+                if (special.hasOwnProperty(key)) {
+                    extra.push(key + "=" + headers[key]);
+                }
+            });
+            if (!headers.hasOwnProperty('Date')) {
+                headers.Date = (new Date()).toUTCString();
+            }
+            headers.Server = "NodeJS " + process.version;
+            headers["X-Runtime"] = Date.now() - start;
+        }
+        console.log("%s - [%s] %s %s %s \"%s\"", req.connection.remoteAddress, headers.Date, req.method, req.url, code, req.headers['user-agent']);
+        res.writeHead = writeHead;
+        res.writeHead(code, headers);
+    };
+    next();
+}, require('wheat')(__dirname + "/.."))).listen(process.env.NODE_DEV ? 8000 : 80);
